138a6da3eac125003a0e31b2e5855f2781223829
samanta2299 <samanta.ndoj@gmail.com>
2022-03-29 11:08:05 +0200

    modifiche

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Stampa");
+        System.out.println("Stampa2");
+        System.out.println("Stampa3");
+        System.out.println("Stampa ripasso");
+        System.out.println("Modifica da collaboratore");
+    }
+}
